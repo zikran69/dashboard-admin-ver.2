d390ffaca2b6a14b3c4e6ba868bd4de65ee8df00
bahe <bahebdg@gmail.com>
2023-12-07 21:58:09 +0700

    update filtering select dinamic

--- src/Components/category/search-category.jsx
+++ src/Components/category/search-category.jsx
@@ -4,7 +4,6 @@ import auth from "../../utils/auth";
 
 export default function SearchCategory({ search }) {
   const [res, setRes] = useState(null);
-  const [nameCategory, setnameCategory] = useState(null);
   useEffect(() => {
     fetch(`${import.meta.env.VITE_ADDR_API}/category`, {
       headers: {
@@ -14,15 +13,9 @@ export default function SearchCategory({ search }) {
       .then((res) => res.json())
       .then((res) => setRes(res.categories));
   }, []);
-  // if (res) {
-  //   setnameCategory(res.map((res) => res.nameCategory));
-  //   const nameCategory = res.map((res) => res.nameCategory);
-  //   console.log(nameCategory);
-  // }
+
   const searching = (e) => {
     search(e.target.value.toLowerCase());
-    setnameCategory(res.map((res) => res.nameCategory));
-    console.log(nameCategory);
   };
   return (
     <div className="md:col-span-3">
@@ -32,9 +25,10 @@ export default function SearchCategory({ search }) {
         className="h-10 border mt-1 rounded-sm px-4 focus:border-blue-400 focus:outline-none"
       >
         <option>All</option>
-        <option value={"Junior Suite"}>Junior Suite</option>
-        <option value={"Executive Suite"}>Executive Suite</option>
-        <option value={"Super Delux"}>Super Delux</option>
+        {res &&
+          res.map(({ nameCategory }, index) => {
+            return <option key={index}>{nameCategory}</option>;
+          })}
       </select>
     </div>
   );
